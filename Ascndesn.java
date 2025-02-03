9b5ed3441bdf825666039d794216d37c392b9b55
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-03 21:34:07 +0530

    Update Ascndesn.java

--- Ascndesn.java
+++ Ascndesn.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Ascndesn {
     static void sort(Integer arr[]) {
         int n = arr.length;
-        Arrays.sort(arr, 0, n / 2);
         Arrays.sort(arr, n / 2, n, Collections.reverseOrder());
     }
 
